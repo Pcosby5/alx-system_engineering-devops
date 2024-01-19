9e01deb79c5e892029dd785418b4cc58473ab2b3
Pcosby5 <pcosby50@gmail.com>
2024-01-19 23:27:00 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# using Puppet Install Flask
+# Using Puppet install FLask
 
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
 
