3cf28a64d453bf79260333c562065d940102739e
Pcosby5 <pcosby50@gmail.com>
2024-01-19 23:19:45 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
-# install_flask package
+# using Puppet Install Flask
 
-package { 'python3-pip':
-  ensure => present,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3'
 }
 
