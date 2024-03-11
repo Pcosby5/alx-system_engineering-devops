a98f890a5d97dc6212ce3f2e3c4b0353c9d365b0
Pcosby5 <pcosby50@gmail.com>
2024-03-11 20:35:41 +0100

    README.md

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,17 @@
 # Puppet manifest to fix Apache 500 error
-# Define resources to manage Apache configuration, packages, and services
-
-# Install or ensure the Apache package is installed
-package { 'apache2':
-  ensure => installed,
+class apache_fix {
+    package { 'apache2':
+        ensure => installed,
+    }
 }
 
-# Manage the Apache configuration file
-file { '/etc/apache2/apache2.conf':
-  ensure  => file,
-  content => template('apache/apache2.conf.erb'),
-  # You may need to replace 'template' with actual content or use a template file
-  notify  => Service['apache2'],
-}
+    file { '/etc/apache2/apache2.conf':
+        ensure  => file,
+        content => template('apache/apache2.conf.erb'),
+        notify  => Service['apache2'],
+    }
 
-# Restart the Apache service after configuration changes
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/apache2/apache2.conf'],
-}
+
+    service { 'apache2':
+        ensure  => running,
+        enable}
